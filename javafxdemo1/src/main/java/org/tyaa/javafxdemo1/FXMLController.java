68701cc0cbb6fc36f572496dc41f9603026abfd4
YuriiTrofimenko <tyaa@ukr.net>
2019-07-06 11:52:17 +0300

    add work with students info

--- javafxdemo1/src/main/java/org/tyaa/javafxdemo1/FXMLController.java
+++ javafxdemo1/src/main/java/org/tyaa/javafxdemo1/FXMLController.java
@@ -2,24 +2,55 @@ package org.tyaa.javafxdemo1;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private TextField firstName;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField lastName;
+    @FXML
+    private TableView studentsTable;
+    @FXML
+    private TableColumn fisrtNameColumn;
+    @FXML
+    private TableColumn lasrtNameColumn;
+    
+    private ObservableList<Student> students;
+    
+    //Обпаботчик клика по кнопке
+    @FXML
+    private void addHandler(ActionEvent event) {
+        // System.out.println(firstName.getText());
+        System.out.println(students);
+        System.out.println(firstName.getText());
+        System.out.println(lastName.getText());
+        students.add(new Student(firstName.getText(), lastName.getText()));
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //Источник данных - наблюдабельный список
+        students = FXCollections.observableArrayList();
+        //Сопоставление: из каких полей объектов "Студент"
+        //в какие колонки таблицы выводить данные
+        fisrtNameColumn.setCellValueFactory(
+                new PropertyValueFactory<Student, String>("firstName")
+        );
+        lasrtNameColumn.setCellValueFactory(
+                new PropertyValueFactory<Student, String>("lastName")
+        );
+        //Подключаем список объектов "Студент" к таблице
+        studentsTable.setItems(students);
     }    
 }
---
+++ javafxdemo1/src/main/java/org/tyaa/javafxdemo1/Student.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tyaa.javafxdemo1;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author student
+ */
+public class Student {
+    
+    public IntegerProperty id;
+    public StringProperty firstName;
+    public StringProperty lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+    }
+
+    public StringProperty getFirstNameProperty() {
+        return firstName;
+    }
+
+    public StringProperty getLastNameProperty() {
+        return lastName;
+    }
+}
